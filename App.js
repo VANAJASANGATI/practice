a19b6c1b40d7332ae1a4fa79e2e40b04b8e73622
Vanaja Sangati <vanaja1073@gmail.com>
2023-06-26 22:20:05 +0530

    first changes

---
+++ App.js
@@ -0,0 +1,16 @@
+
+import Header from './components/Header';
+import UserForm from './components/UserForm';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <Header/>
+    <UserForm/>
+    </>
+  );
+}
+
+export default App;
